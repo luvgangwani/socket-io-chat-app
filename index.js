b8f35c2b11ad063da836e1a9317dea48aaf96903
Luv Gangwani <luvgangwani>
2020-06-27 23:57:21 +1000

    Lesson 3 - Installing socket io

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
 
@@ -6,4 +7,10 @@ app.use(express.static('public'));
 
 const server = app.listen(5000, function() {
     console.log('Server listening at port 5000');
+});
+
+const io = socket(server);
+
+io.on('connection', function(socket){
+    console.log('Socket connection established', socket.id);
 });
